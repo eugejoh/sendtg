2a146e429e8c670aa6f2287af4a793be8dc6d69e
[Eugene Joh] <ejoh@bu.edu>
2019-05-08 09:51:23 -0400

    changed param name issue

--- R/sendtg_file.R
+++ R/sendtg_file.R
@@ -1,6 +1,6 @@
 #'  Send File via Telegram
 #'
-#' @param fpath a single \code{character} string containing the file fpath
+#' @param path a single \code{character} string containing the file fpath
 #' @param ... other arguments fo the \code{sendDocument} function
 #'
 #' @return a message is sent to the chat ID contained in the .Renviron file with the file
